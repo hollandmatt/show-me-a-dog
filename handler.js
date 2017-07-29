dca309ebc918c1cb20fe4aa41ce3278bf8acd514
Matt Holland <hollandmatt@gmail.com>
2017-07-28 19:39:28 -0700

    show working

--- handler.js
+++ handler.js
@@ -3,8 +3,10 @@
 const fetch = require('isomorphic-unfetch');
 const Handlebars = require('handlebars');
 
-const templateSource = require('./template');
-const template = Handlebars.compile(templateSource);
+const templateListSource = require('./template-list');
+const templateList = Handlebars.compile(templateListSource);
+const templateShowSource = require('./template-show');
+const templateShow = Handlebars.compile(templateShowSource);
 
 module.exports.list = (event, context, callback) => {
   fetch('https://dog.ceo/api/breeds/list/all').then((response) => {
@@ -16,7 +18,7 @@ module.exports.list = (event, context, callback) => {
       breeds: breedNames
     };
 
-    const html = template(context);
+    const html = templateList(context);
 
     const response = {
       statusCode: 200,
@@ -31,4 +33,26 @@ module.exports.list = (event, context, callback) => {
 };
 
 module.exports.show = (event, context, callback) => {
+  const breed = event.pathParameters.breed;
+
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`).then((response) => {
+    return response.json();
+  }).then((json) => {
+    const context = {
+      breed,
+      image: json.message
+    };
+
+    const html = templateShow(context);
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html'
+      },
+      body: html
+    };
+
+    callback(null, response);
+  });
 };
---
+++ template-show.js
@@ -0,0 +1,11 @@
+module.exports = `
+<html>
+  <head>
+    <title>Show Me A Dog</title>
+  </head>
+  <body>
+    <h1>Here is your {{breed}}!</h1>
+    <div><img src="{{image}}"></div>
+  </body>
+</html>
+`;
